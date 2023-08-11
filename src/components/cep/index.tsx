4df1ca401a2db1a623cd9bbfdae8863a52ae8228
AlexandreReys <alexandre@mytaverse.com>
2023-08-11 20:24:29 -0300

    change form 2 columns

--- src/components/cep/index.tsx
+++ src/components/cep/index.tsx
@@ -11,8 +11,9 @@ export default function Team() {
           <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
         </div>
       </header>
+
       <main className="mx-auto">
-        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 mt-4 bg-white">
           <h1 className='font-bold'>
             Dashboard Team - Test Form
           </h1>
@@ -21,23 +22,35 @@ export default function Team() {
             <input {...register('address.zipCode')} className={inputStyles} type="text" placeholder='Cep' maxLength={9} />
             {errors.address?.zipCode?.message && (<p className={errorMsgStyles}>{errors.address?.zipCode?.message}</p>)}
 
-            <input {...register('address.street')} className={inputStyles} type="text" placeholder='Rua' />
-            {errors.address?.street?.message && (<p className={errorMsgStyles}>{errors.address?.street?.message}</p>)}
+            <div className="flex -mx-2">
+              <div className="w-3/4 px-2">
+                <input {...register('address.street')} className={inputStyles} type="text" placeholder='Rua' />
+                {errors.address?.street?.message && (<p className={errorMsgStyles}>{errors.address?.street?.message}</p>)}
+              </div>
 
-            <input {...register('address.number')} className={inputStyles} type="text" placeholder='Numero' />
-            {errors.address?.number?.message && (<p className={errorMsgStyles}>{errors.address?.number?.message}</p>)}
+              <div className="w-1/4 px-2">
+                <input {...register('address.number')} className={inputStyles} type="text" placeholder='Numero' />
+                {errors.address?.number?.message && (<p className={errorMsgStyles}>{errors.address?.number?.message}</p>)}
+              </div>
+            </div>
 
             <input {...register('address.district')} className={inputStyles} type="text" placeholder='Bairro' />
             {errors.address?.district?.message && (<p className={errorMsgStyles}>{errors.address?.district?.message}</p>)}
 
             <input {...register('address.complement')} className={inputStyles} type="text" placeholder='Complemento' />
-            {errors.address?.complement?.message && (<p className={errorMsgStyles}>{errors.address?.Complement?.message}</p>)}
+            {errors.address?.complement?.message && (<p className={errorMsgStyles}>{errors.address?.complement?.message}</p>)}
 
-            <input {...register('address.city')} className={inputStyles} type="text" placeholder='Cidade' />
-            {errors.address?.city?.message && (<p className={errorMsgStyles}>{errors.address?.city?.message}</p>)}
+            <div className="flex -mx-2">
+              <div className="w-3/4 px-2">
+                <input {...register('address.city')} className={inputStyles} type="text" placeholder='Cidade' />
+                {errors.address?.city?.message && (<p className={errorMsgStyles}>{errors.address?.city?.message}</p>)}
+              </div>
 
-            <input {...register('address.state')} className={inputStyles} type="text" placeholder='Bairro' />
-            {errors.address?.state?.message && (<p className={errorMsgStyles}>{errors.address?.state?.message}</p>)}
+              <div className="w-1/4 px-2">
+                <input {...register('address.state')} className={inputStyles} type="text" placeholder='Estado' />
+                {errors.address?.state?.message && (<p className={errorMsgStyles}>{errors.address?.state?.message}</p>)}
+              </div>
+            </div>
 
             <button type="submit" className={buttonStyles}>Enviar</button>
           </form>
